331f0c591550f10e191e9046747d666e2f163249
Travis Weir <tweirtx@gmail.com>
2019-08-01 23:09:29 -0500

    Correct Android SDK, forgot to commit a file or 2

---
+++ lib/showdialog.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class dialogShower {
+  void _showDialog(String message, BuildContext context) {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Notice"),
+          content: new Text(message),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
